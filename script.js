3a203198f33b0d109fd968ced6952e23b9271d19
Princegainsight <prince1022raj@gmail.com>
2025-07-17 20:21:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,9 +20,18 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     window.location.href = 'welcome.html';
 });
 
-
+//PX Script
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-VOA4L8B9NFGB-2");
+
+
+//Login Button data
+  document.addEventListener('DOMContentLoaded', function() {
+            const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
+//PX Identify call
+
+
