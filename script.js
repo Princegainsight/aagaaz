bd6b565731d573871c359f27ef51c0067529f001
Princegainsight <prince1022raj@gmail.com>
2025-07-18 00:30:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+//PX Script Tag
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-VOA4L8B9NFGB-2");
+
+
+//PX Identify call
+ aptrinsic("identify",
+  {
+  //User Fields
+    "id": loggedInUser.email, // Required for logged in app users
+    "email": loggedInUser.email,
+    "firstName": loggedInUser.name,
+    "lastName": "Raj",
+    "signUpDate": 1522697426479, //unix time in ms
+    "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+    "price" : 95.5,
+    "userHash": "" // optional transient for HMAC identification
+  },
+  {
+  //Account Fields
+    "id":"Adobe", //Required
+    "name":"Adobe System",
+    "Program": "Platinum" // flat custom attributes
+ });
+
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
 
@@ -17,38 +45,6 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     // Redirect to welcome page
     window.location.href = 'welcome.html';
-});
-
-//PX Script Tag
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-VOA4L8B9NFGB-2");
-
-
-//Login Button data and PX Identify call
-  document.addEventListener('DOMContentLoaded', function() {
-            const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-document.getElementById('userName').textContent = loggedInUser.name || 'Guest';
- aptrinsic("identify",
-  {
-  //User Fields
-    "id": loggedInUser.email, // Required for logged in app users
-    "email": loggedInUser.email,
-    "firstName": loggedInUser.name,
-    "lastName": "Raj",
-    "signUpDate": 1522697426479, //unix time in ms
-    "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
-    "price" : 95.5,
-    "userHash": "" // optional transient for HMAC identification
-  },
-  {
-  //Account Fields
-    "id":"Adobe", //Required
-    "name":"Adobe System",
-    "Program": "Platinum" // flat custom attributes
- });
 });
 
  // Fallback in case name is not available
