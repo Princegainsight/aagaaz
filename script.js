0a855c9c9b4e5d9d576c7fbfe2a278bdb33678a6
Princegainsight <prince1022raj@gmail.com>
2025-07-18 00:34:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //PX Script Tag
+document.addEventListener('DOMContentLoaded', function() {
+            const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
@@ -46,6 +49,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     // Redirect to welcome page
     window.location.href = 'welcome.html';
 });
+      });
 
  // Fallback in case name is not available
 
